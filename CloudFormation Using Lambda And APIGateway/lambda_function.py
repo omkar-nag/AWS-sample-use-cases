957a852549a3a9c2725fac1908cec9e585b59815
OmkarNag <72746084+omkar-nag@users.noreply.github.com>
2021-06-28 13:02:39 +0530

    Update lambda_function.py

--- CloudFormation Using Lambda And APIGateway/lambda_function.py
+++ CloudFormation Using Lambda And APIGateway/lambda_function.py
@@ -5,17 +5,20 @@ client = boto3.client('cloudformation')
 
 def lambda_handler(event, context):
 
-
     method = event["requestContext"]["http"]["method"]
 
     if method == "POST":
+        
         data = event["body"]
+        
         try:
+            
             data = json.loads(data)
             
             stack_name,stack_template = data["name"], json.loads(data["template"])
         
         except:
+        
             return{ 'statusCode': 200, 'body': json.dumps("Invalid JSON") }
             
         response = client.create_stack(
